6f94cd82f73166feda5c1c9edcb4cf1dc38d0a1a
MOMO320 <yoodud@naver.com>
2020-02-04 17:44:53 +0900

    Item class Implementation

--- 01. PocketMonster/src/Game/GameObject/Item.h
+++ 01. PocketMonster/src/Game/GameObject/Item.h
@@ -1,10 +1,34 @@
 #pragma once
 #include "Object.h"
 
+
+
 class Item
 	: public Object
 {
 protected:
 	std::string m_name;
 	ITEM_TYPE m_type;
+
+public:
+	virtual bool init() override;
+	virtual void update(float _deltaTime) override;
+
+	virtual void render(HDC hdc) override;
+	virtual void afterRender(HDC hdc) override;
+	virtual void debugRender(HDC hdc) override;
+
+private:
+	Image*			m_img;				//이미지
+	UTIL::IRECT		m_iRect;			//상자(좌표값)
+	std::string		m_name;				//이름
+	int				m_count;			//개수
+	int				m_price;			//가격
+	int				m_healHp;			//Hp회복량
+	int				m_hitDamage;		//데미지량
+	int				m_addAtk;			//공격력 
+	int				m_catchPercentage;	//포켓몬 잡는 확률
+	std::string		m_description;		//설명
 };
+
+// 2020.02.04 item 클래스 상속해주는 클래스로 변경하기
--- 01. PocketMonster/src/Game/GameTypes.h
+++ 01. PocketMonster/src/Game/GameTypes.h
@@ -2,17 +2,29 @@
 
 enum class ITEM_TYPE
 {
-	ITEM_TYPE_RECOVERY,
-	ITEM_TYPE_BALL,
-	ITEM_TYPE_IMPORTANT,
-	ITEM_TYPE_SKIL_MACHINE,
-	ITEM_TPYE_EQUIPABLE
+	ITEM_TYPE_RECOVERY = 0,			 // 회복템
+	ITEM_TYPE_BALL,				     // 포켓몬볼
+	ITEM_TYPE_IMPORTANT,		     // 중요한 아이템??
+	ITEM_TYPE_SKILL_MACHINE,		     // 스킬 머신
+	ITEM_TPYE_EQUIPABLE,			 // 물건 장착
+	ITEM_TYPE_ABILITY_POTION,		 // 능력 향상 포션
+	DEFAULT
 };
+
 
 enum class POCKETMON_ATTIBUTE
 {
-	POCKETMON_ATTIBUTE_FIRE,
-	POCKETMON_ATTIBUTE_STONE,
+	POCKETMON_ATTIBUTE_FIRE,	 
+	POCKETMON_ATTIBUTE_STONE,	 
 	POCKETMON_ATTIBUTE_WATER,
 	POCKETMON_ATTIBUTE_GRASS
+};
+
+enum class SKILL
+{
+	SCRACH ,                                                                                                                                                                                                                                                                                                                                                                                                                                 
+	HOWL ,
+	FIRE ,
+	SKILL_MACHINE_1 ,
+	SKILL_MACHINE_2
 };
