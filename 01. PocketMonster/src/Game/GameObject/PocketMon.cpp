928586a6adde20fbb4aa331a7053eb93d9d237b2
PTHParkTaeHwan <pth0810@naver.com>
2020-02-05 23:12:17 +0900

    포켓몬클래스 수정중

---
+++ 01. PocketMonster/src/Game/GameObject/PocketMon.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "PocketMon.h"
+
+bool PocketMon::init()
+{
+	return false;
+}
+
+void PocketMon::update(float _deltaTime)
+{
+}
+
+void PocketMon::render(HDC hdc)
+{
+}
+
+void PocketMon::afterRender(HDC hdc)
+{
+}
+
+void PocketMon::debugRender(HDC hdc)
+{
+}
